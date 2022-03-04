59442b7783e5808d7a5a8eb487bd0ea4452ae77f
unknown <ramsey479@gmail.com>
2022-03-04 08:22:27 -0600

    New Ultimate commit

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
 //Author: Samy Ramsey for Ultimate.ai
 import fetch from "node-fetch";
+var objj;
 
 //Helper function. Uses the Fetch API to retrieve JSON from a specific URL, then parses it.
-function getJSON(url) {
-  fetch(url, { 
+async function getJSON(url) {
+  return fetch(url, { 
     method: 'GET'
   })
-  .then(response => response.json())
-  .then(function(json) {
-    //The below extracts the relevant parts of the JSON, parsing it for output, it is then logged and returned.
-    var weather = json.main.temp;
+  .then(data => data.json())
+  .then (data => {
+    var weather = data.main.temp;
     var text = '{"params":{"name":"temperature", "value":' + weather +'}}';
-    //Output text is converted into a JSON object, then output to the console.
     var obj = JSON.parse(text);
-    console.log(obj);
     return obj;
-    });
+    //console.log(obj);
+  })
 }
 
 //Main function. Takes a city and country in the world, and returns JSON in specified format written in the the Ultimate challenge documentation.
@@ -28,9 +27,10 @@ function weatherRetriever(city, country) {
   var api_key = "&appid=" + "64b1b5bd5faf3696c0d2b0b06b3b058d";
   var unit = "&units=metric";
   var current_weather = api + city + country + api_key + unit;
-  //The URL used by OpenWeatherMap's API is output to the console.
-  console.log("Full API URL: " + current_weather);
-  return getJSON(current_weather);
+  getJSON(current_weather).then(function(result) {
+    console.log(result);
+    console.log("Full API URL: " + current_weather);
+    return result;
+  });
 }
-
 weatherRetriever("Berlin", "DE");
